45bf43e6e0b7fa41c36b9e21155be0493d96e005
williamtong0228 <38699976+williamtong0228@users.noreply.github.com>
2023-09-13 10:52:26 +0800

    Create CalendarEventStore.swift

---
+++ CalendarEventStore.swift
@@ -0,0 +1,47 @@
+//
+//  CalendarEvents.swift
+//  PersonalHoroscope
+//
+//  Created by William Seele on 2023/9/11.
+//
+
+import Foundation
+import CoreLocation
+
+
+class CalendarEventManager:ObservableObject{
+    
+    @Published var events:[CalendarEventStruct] = []
+    let calendarEndDays = 7
+    func generateEvents(viewModel:ChartViewModel, sharing:SharedValues){
+        DispatchQueue.main.asyncAfter(deadline: .now()){
+          self.events = []
+          
+            
+          let currentTime = Date()
+            
+          var myId = 0
+            
+          for i in 0...Int(calendarEndDays){
+            
+            let idx = Int(calendarEndDays) - i
+                
+              //...    
+              //...
+              //...
+              //... 
+              self.events.append(CalendarEventStruct(id: myId, date: sharing.eventTime, eventData: viewModel.processedEventData))
+              
+              myId += 1
+
+                
+          
+          }
+        
+        }
+        
+    }
+ 
+    
+    
+}
